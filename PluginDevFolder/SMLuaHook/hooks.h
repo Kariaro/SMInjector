f2f82f5d8163f80a7165e53abee1ef308859b619
TechnologicNick <technologicnick@gmail.com>
2021-09-07 00:22:06 +0200

    Add SMLuaHook

---
+++ PluginDevFolder/SMLuaHook/hooks.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <stdio.h>
+#include <hook.h>
+#include <lua.hpp>
+
+#include <console.h>
+using Console::Color;
+
+
+// LUAL_REGISTER
+typedef void (*pluaL_register)(lua_State*, const char*, const luaL_Reg*);
+Hook* hck_luaL_register;
+
+// LUAL_LOADSTRING
+typedef int (*pluaL_loadstring)(lua_State*, const char*);
+Hook* hck_luaL_loadstring;
+
+// LUA_NEWSTATE
+typedef lua_State* (*plua_newstate)(lua_Alloc, void*);
+Hook* hck_lua_newstate;
+
+// LUAL_LOADBUFFER
+typedef int (*pluaL_loadbuffer)(lua_State*, const char*, size_t, const char*);
+Hook* hck_luaL_loadbuffer;
+
+// =============
+
+namespace Hooks {
+	void hook_luaL_register(lua_State* L, const char* libname, const luaL_Reg* l) {
+		Console::log(Color::Aqua, "hook_luaL_register: libname=[%s]", libname);
+		return ((pluaL_register)hck_luaL_register->Gate())(L, libname, l);
+	}
+
+	int hook_luaL_loadstring(lua_State* L, const char* s) {
+		Console::log(Color::Aqua, "hook_luaL_loadstring: s=[ ... ]");
+		return ((pluaL_loadstring)hck_luaL_loadstring->Gate())(L, s);
+	}
+
+	lua_State* hook_lua_newstate(lua_Alloc f, void* ud) {
+		Console::log(Color::Aqua, "hck_lua_newstate: ud=[%p]", ud);
+		return ((plua_newstate)hck_lua_newstate->Gate())(f, ud);
+	}
+
+	int hook_luaL_loadbuffer(lua_State* L, const char* buff, size_t sz, const char* name) {
+		Console::log(Color::Aqua, "hck_luaL_loadbuffer: buff=[ ... ], sz=[%zu], name=[%s]", sz, name);
+		return ((pluaL_loadbuffer)hck_luaL_loadbuffer->Gate())(L, buff, sz, name);
+	}
+}
---
+++ PluginDevFolder/SMLuaHook/main.cpp
@@ -0,0 +1,59 @@
+#define _SM_LIBRARY_BUILD_PLUGIN
+#define _SM_PLUGIN_NAME SMLuaHook
+
+#include <sm_lib.h>
+#include <console.h>
+using Console::Color;
+
+#include "hooks.h"
+bool InjectLua();
+
+LIB_RESULT PluginLoad() {
+	Console::log(Color::Aqua, "Starting plugin ...");
+
+	if (!InjectLua()) {
+		Console::log(Color::Red, "Failed to inject lua functions");
+		return PLUGIN_ERROR;
+	}
+
+	return PLUGIN_SUCCESSFULL;
+}
+
+HookUtility* util;
+bool InjectLua() {
+	Console::log(Color::Aqua, "Installing lua hooks");
+
+	util = new HookUtility();
+	hck_luaL_register = util->InjectFromName("lua51.dll", "luaL_register", &Hooks::hook_luaL_register, 15);
+	hck_luaL_loadstring = util->InjectFromName("lua51.dll", "luaL_loadstring", &Hooks::hook_luaL_loadstring, 16);
+	hck_lua_newstate = util->InjectFromName("lua51.dll", "lua_newstate", &Hooks::hook_lua_newstate, -14, 6);
+	hck_luaL_loadbuffer = util->InjectFromName("lua51.dll", "luaL_loadbuffer", &Hooks::hook_luaL_loadbuffer, 17, 13);
+
+	if (!hck_luaL_register) {
+		Console::log(Color::Red, "Failed to inject 'luaL_register'");
+		return false;
+	}
+
+	if (!hck_luaL_loadstring) {
+		Console::log(Color::Red, "Failed to inject 'luaL_loadstring'");
+		return false;
+	}
+
+	if (!hck_lua_newstate) {
+		Console::log(Color::Red, "Failed to inject 'lua_newstate'");
+		return false;
+	}
+
+	if (!hck_luaL_loadbuffer) {
+		Console::log(Color::Red, "Failed to inject 'luaL_loadbuffer'");
+		return false;
+	}
+
+	return true;
+}
+
+LIB_RESULT PluginUnload() {
+	Console::log(Color::Aqua, "Unloading this plugin!");
+	util->Unload();
+	return PLUGIN_SUCCESSFULL;
+}
