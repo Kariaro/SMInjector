035f24f88cf15177a047aa0db706fe70692ee011
TechnologicNick <technologicnick@gmail.com>
2021-09-10 00:19:07 +0200

    Add Console::wlog

--- SMLibrary/include/console.h
+++ SMLibrary/include/console.h
@@ -37,8 +37,8 @@ namespace Console {
 	};
 	
 #ifdef _SM_LIBRARY_BUILD_PLUGIN
-	_LIB_IMPORT
-	void vlogf(Color color, const char *plugin_name, const char *format, va_list args);
+	_LIB_IMPORT void vlogf(Color color, const char* plugin_name, const char* format, va_list args);
+	_LIB_IMPORT void wvlogf(Color color, const char* plugin_name, const wchar_t* format, va_list args);
 #else
 	FILE* console_handle = NULL;
 	HANDLE hConsole;
@@ -59,8 +59,7 @@ namespace Console {
 #endif
 	}
 
-	_LIB_EXPORT
-	void vlogf(Color color, const char *plugin_name, const char *format, va_list args) {
+	_LIB_EXPORT void vlogf(Color color, const char *plugin_name, const char *format, va_list args) {
 		if(!console_handle) return;
 
 		if(!hConsole) hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -70,11 +69,28 @@ namespace Console {
 		vprintf(format, args);
 		printf("\n");
 	}
+
+	_LIB_EXPORT void wvlogf(Color color, const char* plugin_name, const wchar_t* format, va_list args) {
+		if (!console_handle) return;
+
+		if (!hConsole) hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		if (hConsole) SetConsoleTextAttribute(hConsole, (WORD)color);
+
+		printf("[%s]: ", plugin_name);
+		vwprintf(format, args);
+		printf("\n");
+	}
 #endif
 	void log(Color color, const char *format, ...) {
 		va_list args;
 		va_start(args, format);
 		vlogf(color, _LIB_PLUGIN_NAME_STR, format, args);
+	}
+
+	void wlog(Color color, const wchar_t* format, ...) {
+		va_list args;
+		va_start(args, format);
+		wvlogf(color, _LIB_PLUGIN_NAME_STR, format, args);
 	}
 }
 
