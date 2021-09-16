f4ff1e93dcb4938bc14df80e6e07e6ec7b9fbd53
TechnologicNick <technologicnick@gmail.com>
2021-09-16 22:50:29 +0200

    Improve logging of luaL_register for SMLuaReaderPlugin

--- PluginDevFolder/SMLuaReaderPlugin/src/hooks.h
+++ PluginDevFolder/SMLuaReaderPlugin/src/hooks.h
@@ -1,6 +1,7 @@
+#pragma once
 #include <stdio.h>
 #include <gamehook.h>
-#include "lua.h"
+#include <lua.hpp>
 
 #include <console.h>
 using Console::Color;
@@ -12,35 +13,42 @@ GameHook* hck_luaL_register;
 
 // LUAL_LOADSTRING
 typedef int (*pluaL_loadstring)(lua_State*, const char*);
-GameHook *hck_luaL_loadstring;
+GameHook* hck_luaL_loadstring;
 
 // LUA_NEWSTATE
-typedef lua_State *(*plua_newstate)(lua_Alloc, void*);
-GameHook *hck_lua_newstate;
+typedef lua_State* (*plua_newstate)(lua_Alloc, void*);
+GameHook* hck_lua_newstate;
 
 // LUAL_LOADBUFFER
 typedef int (*pluaL_loadbuffer)(lua_State*, const char*, size_t, const char*);
-GameHook *hck_luaL_loadbuffer;
+GameHook* hck_luaL_loadbuffer;
 
 // =============
 
 namespace Hooks {
-	void hook_luaL_register(lua_State *L, const char *libname, const luaL_Reg *l) {
+	void hook_luaL_register(lua_State* L, const char* libname, const luaL_Reg* l) {
 		Console::log(Color::Aqua, "hook_luaL_register: libname=[%s]", libname);
+
+		const luaL_Reg* ptr = l;
+
+		for (int i = 0; ptr->name != NULL; i++, ptr++) {
+			Console::log(Color::Aqua, "hook_luaL_register: luaL_Reg[%d] name=[%s] func=[%p]", i, ptr->name, (void*)ptr->func);
+		}
+
 		return ((pluaL_register)*hck_luaL_register)(L, libname, l);
 	}
 
-	int hook_luaL_loadstring(lua_State *L, const char *s) {
+	int hook_luaL_loadstring(lua_State* L, const char* s) {
 		Console::log(Color::Aqua, "hook_luaL_loadstring: s=[ ... ]");
 		return ((pluaL_loadstring)*hck_luaL_loadstring)(L, s);
 	}
 
-	lua_State *hook_lua_newstate(lua_Alloc f, void* ud) {
+	lua_State* hook_lua_newstate(lua_Alloc f, void* ud) {
 		Console::log(Color::Aqua, "hck_lua_newstate: ud=[%p]", ud);
 		return ((plua_newstate)*hck_lua_newstate)(f, ud);
 	}
 
-	int hook_luaL_loadbuffer(lua_State *L, const char *buff, size_t sz, const char *name) {
+	int hook_luaL_loadbuffer(lua_State* L, const char* buff, size_t sz, const char* name) {
 		Console::log(Color::Aqua, "hck_luaL_loadbuffer: buff=[ ... ], sz=[%zu], name=[%s]", sz, name);
 		return ((pluaL_loadbuffer)*hck_luaL_loadbuffer)(L, buff, sz, name);
 	}
--- PluginDevFolder/SMLuaReaderPlugin/src/lua.h
+++
@@ -1,7 +0,0 @@
-typedef void *(*lua_Alloc) (void *ud, void *ptr, size_t osize, size_t nsize);
-typedef struct lua_State lua_State;
-typedef int (*lua_CFunction) (lua_State *L);
-typedef struct luaL_Reg {
-	const char *name;
-	lua_CFunction func;
-} luaL_Reg;
